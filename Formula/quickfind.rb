c9cb60a202079f37154d1d8ee00f89ee93ec9223
Aditya Baindur <adityabaindur@icloud.com>
2025-01-31 04:11:39 -0500

    version number

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -2,12 +2,12 @@ class Quickfind < Formula
   desc "A description of your quickFind script"
   homepage "https://github.com/Aditya-Baindur/quickFind"
   url "https://raw.githubusercontent.com/Aditya-Baindur/quickFind/main/cmd.sh"  # Direct link to the raw script
-  sha256 "34052e7bab22fd994268090324cd6eb837d657daad1d50fb402925b59cb175e6"  # Use the actual checksum of the raw script
+  version "1.0.0"  # Add a version number here
+  sha256 "34052e7bab22fd994268090324cd6eb837d657daad1d50fb402925b59cb175e6"  # Your script's checksum
 
-  depends_on "zsh"  # If required, to run the script
+  depends_on "zsh"  # If required to run the script
 
   def install
-    bin.install "cmd.sh"  # This installs the script to the bin directory
+    bin.install "cmd.sh"  # Install the script to the bin directory
   end
 end
-
