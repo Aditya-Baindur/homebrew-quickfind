3134fde06c8524ef6e9c52dd1bdaecb0ffb9c14c
Aditya Baindur <adityabaindur@icloud.com>
2025-01-31 04:10:12 -0500

    bug fix

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -1,13 +1,13 @@
 class Quickfind < Formula
-    desc "A description of your quickFind script"
-    homepage "https://github.com/Aditya-Baindur/quickFind"
-    url "https://github.com/Aditya-Baindur/quickFind/archive/v1.0.0.tar.gz"  # Adjust to your specific version URL
-    sha256 "34052e7bab22fd994268090324cd6eb837d657daad1d50fb402925b59cb175e6"  # Replace with the actual checksum from `shasum`
-  
-    depends_on "zsh"  # Any necessary dependencies
-  
-    def install
-      bin.install "cmd.sh"  # Path to your script file
-    end
+  desc "A description of your quickFind script"
+  homepage "https://github.com/Aditya-Baindur/quickFind"
+  url "https://raw.githubusercontent.com/Aditya-Baindur/quickFind/main/cmd.sh"  # Direct link to the raw script
+  sha256 "34052e7bab22fd994268090324cd6eb837d657daad1d50fb402925b59cb175e6"  # Use the actual checksum of the raw script
+
+  depends_on "zsh"  # If required, to run the script
+
+  def install
+    bin.install "cmd.sh"  # This installs the script to the bin directory
   end
-  
+end
+
