68b998b5352a25c50e60d0fdbab70253fd8c8190
Aditya Baindur <adityabaindur@icloud.com>
2025-01-31 04:14:34 -0500

    vn

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -2,6 +2,7 @@ class Quickfind < Formula
   desc "A description of your quickFind script"
   homepage "https://github.com/Aditya-Baindur/quickFind"
   url "https://raw.githubusercontent.com/Aditya-Baindur/quickFind/main/cmd.sh"  # Direct link to the raw script
+  version "1.0.0"  # Assigning a fixed version
   sha256 "34052e7bab22fd994268090324cd6eb837d657daad1d50fb402925b59cb175e6"  # Your script's checksum
 
   depends_on "zsh"  # If required to run the script
