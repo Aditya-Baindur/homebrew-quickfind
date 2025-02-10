a81b91d72203971a19e4b732c7584df85c882c36
Aditya <111638064+Aditya-Baindur@users.noreply.github.com>
2025-02-10 00:35:47 -0500

    Update quickfind.rb

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -2,7 +2,7 @@ class Quickfind < Formula
   desc "A script to quickly find and search through files"
   homepage "https://github.com/Aditya-Baindur/quickFind"
   url "https://github.com/Aditya-Baindur/quickFind/archive/refs/tags/1.0.0.tar.gz"
-  version "1.0.0"
+  version "2.0.0"
   sha256 "2dbc0b4755acf96b3593d43ba98471d1b23ad5cc23d6d4d8f1312493401ffb77"
 
   depends_on "zsh"  # Optional: Only if required for script execution
