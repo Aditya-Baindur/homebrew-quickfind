cf1d5234ebf77ffd9e94379ba15d1572bd89d38d
Aditya <111638064+Aditya-Baindur@users.noreply.github.com>
2025-02-10 02:05:36 -0500

    Update quickfind.rb
    Changed this formula file for version 3.0.0 of the program

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -1,9 +1,9 @@
 class Quickfind < Formula
   desc "A script to quickly find and search through files"
   homepage "https://github.com/Aditya-Baindur/quickFind"
-  url "https://github.com/Aditya-Baindur/quickFind/archive/refs/tags/2.0.0.tar.gz"
-  version "2.0.0"
-  sha256 "982e7dec505d7a466bf7dbb8ec1e8a4dc8c8f1863f44f9f4715efd1291494061"
+  url "https://github.com/Aditya-Baindur/quickFind/archive/refs/tags/3.0.0.tar.gz"
+  version "3.0.0"
+  sha256 "5744a281b2fb9a3df86b120a76f6db2af1de41c7ff078de9ff382740f3d7b83f"
   
   depends_on "zsh"
 
