8fdd21f0f07e587bef96507c56f2641242113168
Aditya Baindur <adityabaindur@icloud.com>
2025-07-01 13:15:10 -0400

    updated version

--- Formula/quickfind.rb
+++ Formula/quickfind.rb
@@ -1,11 +1,11 @@
 class Quickfind < Formula
-  desc "A script to quickly find and search through files"
+  desc "A script to quickly navigate through files and folders"
   homepage "https://github.com/Aditya-Baindur/quickFind"
-  url "https://github.com/Aditya-Baindur/quickFind/archive/refs/tags/3.0.0.tar.gz"
-  version "3.0.0"
+  url "https://github.com/Aditya-Baindur/quickFind/archive/refs/tags/4.0.tar.gz"
+  version "4.0"
   sha256 "5744a281b2fb9a3df86b120a76f6db2af1de41c7ff078de9ff382740f3d7b83f"
   
-  depends_on "zsh"
+  depends_on "bash"
 
   def install
     bin.install "cmd.sh" => "quickfind"  # Rename and install the script
